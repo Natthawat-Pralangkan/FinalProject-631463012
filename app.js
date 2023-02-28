3ea438dc84eaf443502c6284286cd953777a6fdd
Natthawat Pralangkan <120998129+Natthawat-Pralangkan@users.noreply.github.com>
2023-02-28 12:37:26 +0700

    new

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const sqlite3 = require("sqlite3").verbose();
-const { v4: uuidv4 } = require("uuid");
 // Server configuration
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -123,44 +122,35 @@ app.post("/home", (req, res) => {
   });
 });
 //////webcam/////////
-app.get("/room", (req, res) => {
-  res.redirect(`/${uuidv4()}`);
-});
-
-app.get("/:room", (req, res) => {
-  res.render("room", { roomId: req.params.room });
-});
-// const session = require("express-session");
-const console = require("console");
 const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
 const { ExpressPeerServer } = require("peer");
-app.use(express.urlencoded({ extended: true }));
+const opinions = {
+  debug: true,
+};
+app.set("view engine", "ejs");
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
   },
 });
-// Create peerjs server instance and wrap it with http.Server
-const peerServer = ExpressPeerServer(server);
-app.use("/peerjs", peerServer);
-
+app.use("/peerjs", ExpressPeerServer(server, opinions));
 app.use(express.static("public"));
-
-// Handle socket.io events
+app.get("/room", (req, res) => {
+  res.redirect(`/${uuidv4()}`);
+});
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
 io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId, userName) => {
     socket.join(roomId);
-
     setTimeout(() => {
       socket.to(roomId).broadcast.emit("user-connected", userId);
-    }, 5000);
-
+    }, 1000);
     socket.on("message", (message) => {
       io.to(roomId).emit("createMessage", message, userName);
     });
   });
 });
-server.listen(process.env.PORT || 3030, () => {
-  console.log("Server started on port 3030");
-});
-////////////////////
+server.listen(process.env.PORT || 3030);
